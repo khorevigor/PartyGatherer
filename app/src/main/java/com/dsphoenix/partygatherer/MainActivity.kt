b1b9a1c06d292ec4ac848a51221af4dc705de990
Ihar Khorau <dostup.96@gmail.com>
2022-08-04 17:30:13 +0300

    Initial commit

---
+++ app/src/main/java/com/dsphoenix/partygatherer/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dsphoenix.partygatherer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
