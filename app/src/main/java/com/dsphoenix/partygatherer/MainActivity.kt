d331d209fc12a6551683aa83dcdb6ae323d50a8e
Ihar Khorau <dostup.96@gmail.com>
2022-09-04 17:58:22 +0300

    Setup navigation with Nav component

--- app/src/main/java/com/dsphoenix/partygatherer/MainActivity.kt
+++ app/src/main/java/com/dsphoenix/partygatherer/MainActivity.kt
@@ -3,7 +3,11 @@ package com.dsphoenix.partygatherer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.dsphoenix.partygatherer.databinding.ActivityMainBinding
 import com.dsphoenix.partygatherer.ui.add_edit_event.AddEditEventFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,10 +15,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        supportFragmentManager.commit {
-            replace(R.id.container, AddEditEventFragment())
-        }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
